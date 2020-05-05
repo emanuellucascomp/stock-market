ddad81dcd85414c01652564a4afd6557fdb9f81e
emanuel <cardgameheritage@gmail.com>
2020-05-05 20:07:38 -0300

    assert do retorno

--- test_app.py
+++ test_app.py
@@ -12,11 +12,13 @@ def client():
 def test_search_stock_response_200(client):
     response = client.get('/search?keywords=IBM')
     assert response.status_code == 200
+    assert len(response.get_json().get("bestMatches")) > 0
 
 
-def test_get_stock_response_200(client):
+def test_get_stock_response(client):
     response = client.get('/stock?symbol=IBM')
     assert response.status_code == 200
+    assert len(response.get_json().get("Meta Data")) == 5
 
 
 
